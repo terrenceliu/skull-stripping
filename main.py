16819ae414df109e5ad8a4ed9ce554505186a1cc
TerrenceLiu <yl124@rice.edu>
2018-06-25 17:02:43 -0500

    pipeline grid search

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from model import *
 from data import *
 import time
+import sys
 
-def train(trian_path, img_folder, mask_folder, weight_path, epochs, steps):
+def train(trian_path, img_folder, mask_folder, weight_path, model_num, epochs, steps):
     data_gen_args = dict(rotation_range=0.2,
                          width_shift_range=0.05,
                          height_shift_range=0.05,
@@ -15,17 +16,21 @@ def train(trian_path, img_folder, mask_folder, weight_path, epochs, steps):
                             mask_folder,
                             data_gen_args, save_to_dir=None)
 
-    model = unet()
-    model_checkpoint = ModelCheckpoint(weight_path, monitor='loss', verbose=1, save_best_only=True)
-    model.fit_generator(seq, steps_per_epoch=steps, epochs=epochs, callbacks=[model_checkpoint])
+    model = unet() if model_num == 0 else unet2()
 
-def test(input_path, output_path, weight_path, postfix):
+    model_checkpoint = ModelCheckpoint(weight_path, monitor='loss', verbose=1, save_best_only=True)
+    tensorboard = TensorBoard(histogram_freq=1)
+    csv_logger = CSVLogger('%d_ep%d_stp%d.log' % (model_num, epochs, steps))
+    model.fit_generator(seq, steps_per_epoch=steps, epochs=epochs, callbacks=[model_checkpoint, tensorboard, csv_logger])
+
+
+def test(input_path, output_path, weight_path, model_num, postfix):
     seq = testGenerator(input_path)
 
-    model = unet()
+    model = unet() if model_num == 0 else unet2()
     model.load_weights(weight_path)
 
-    steps = -1
+    steps = 0
 
     for root, dirs, files in os.walk(input_path):
         for file in files:
@@ -42,43 +47,55 @@ if __name__ == '__main__':
     train_folder = "origin"
     mask_folder = "mask"
 
-    test_path = ".\\data\\train_data\\test"
-    out_path = ".\\data\\train_data\\test"
 
-    if not os.path.exists(out_path):
-        os.mkdir(out_path)
 
     """
     Hyper Param
     """
-    epochs = 5
-    steps = 2000
+    # epochs = 5
+    # steps = 2000
+    # model_num = 1
+    epochs_list = [2, 3, 5, 8]
+    steps_list = [500, 1000, 1500, 2000, 3000]
+    for epochs in epochs_list:
+        for steps in steps_list:
+            for model_num in [0, 1]:
+                postfix = "_%d_ep%d_stp%d" % (model_num, epochs, steps)
 
-    postfix = "_ep5_stp2000_bientro"
+                weight_path = ".\\data\\weights\\unet" + postfix + ".h5"
 
-    weight_path = ".\\data\\weights\\unet_epoch" + postfix + ".h5"
+                """
+                Train
+                """
+                train_start_time = time.time()
 
-    """
-    Train
-    """
-    train_start_time = time.time()
+                train(train_path, train_folder, mask_folder, weight_path, model_num, epochs, steps)
 
-    # train(train_path, train_folder, mask_folder, weight_path, epochs)
-    train_end_time = time.time()
+                train_end_time = time.time()
 
-    print("*" * 30)
-    print("Training Time: %.3f"% (train_end_time - train_start_time))
-
-
-    """
-    Test
-    """
-    test_start_time = time.time()
+                print("*" * 30)
+                print("Training Time: %.3f"% (train_end_time - train_start_time))
 
 
-    test(test_path, out_path, weight_path, postfix)
+                """
+                Test
+                """
+                """
+                File Path
+                """
+                test_path = ".\\data\\train_data\\test"
+                out_path = ".\\data\\train_data\\test" + postfix
 
-    test_end_time = time.time()
-    print("Test Time: %.3f" % (test_end_time - test_start_time))
+                if not os.path.exists(out_path):
+                    os.mkdir(out_path)
+                test_start_time = time.time()
+
+                try:
+                    test(test_path, out_path, weight_path, model_num, postfix)
+                except:
+                    print(sys.exc_info())
+
+                test_end_time = time.time()
+                print("Test Time: %.3f" % (test_end_time - test_start_time))
 
 
--- model.py
+++ model.py
@@ -3,20 +3,10 @@ import os
 
 
 from keras.models import Model
-from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, UpSampling2D, Dropout
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Conv2DTranspose
 from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, TensorBoard, CSVLogger
 from keras import backend as K
-
-def dice_coef(y_true, y_pred, smooth=1):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-
-
-def dice_coef_loss(y_true, y_pred):
-    return -dice_coef(y_true, y_pred)
 
 def unet(input_size=(256, 256, 1), pretrained_weights=None):
     """
@@ -84,5 +74,62 @@ def unet(input_size=(256, 256, 1), pretrained_weights=None):
 
     if (pretrained_weights):
         model.load_weights(pretrained_weights)
+
+    return model
+
+
+def dice_coef(y_true, y_pred):
+    smooth = 1
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+def unet2(img_rows=256, img_cols=256, pretrained_weights=None):
+    inputs = Input((img_rows, img_cols, 1))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
 
     return model
